15365c2a3e625bc6d9e9d2bcab869ca1d9554660
Dylan Quiroga Arancibia <dylan.quiroga@userena.cl>
2024-10-27 22:51:36 -0300

    Pagina 404
    Ante algún error de url, desplegará una página con una decoración más amigable para el usuario

--- auth/login.py
+++ auth/login.py
@@ -6,6 +6,10 @@ app.secret_key = 'supersecretkey'  # pa que funcione `flash`
 USERNAME = 'admin'
 PASSWORD = '1234'
 
+@app.route('/')
+def index():
+    return redirect(url_for('login'))
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -17,5 +21,9 @@ def login():
             flash("Nombre de usuario o contraseña incorrectos")
     return render_template('login.html')
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
--- server/app.py
+++ server/app.py
@@ -16,5 +16,9 @@ def home():
     handler = ApiHandler() # Probablemente no vaya aca pero la idea es que se instancie asi
     return render_template('index.html')
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
